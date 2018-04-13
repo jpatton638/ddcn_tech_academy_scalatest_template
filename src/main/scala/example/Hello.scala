d8386d3ab1d4f233ea995c91cf365b952a9219ca
jpatton638 <jpatton638@gmail.com>
2018-04-13 12:35:42 +0100

    Kata improved upon reflection

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,9 +1,23 @@
 package example
 
-object Hello extends Greeting with App {
-  println(greeting)
+object Hello extends App {
+
+  def crossover(first: Chromosome, second: Chromosome, index: Int): List[String] = {
+
+    val firstCombination = first.getStart(index).concat(second.getEnd(index))
+    val secondCombination = second.getStart(index).concat(first.getEnd(index))
+
+    List(firstCombination, secondCombination)
+  }
 }
 
-trait Greeting {
-  lazy val greeting: String = "hello"
+case class Chromosome(dna: String) {
+
+  def getStart(index: Int): String = {
+    dna.substring(0, index)
+  }
+
+  def getEnd(index: Int): String = {
+    dna.substring(index)
+  }
 }
--- src/test/scala/example/HelloSpec.scala
+++ src/test/scala/example/HelloSpec.scala
@@ -2,8 +2,17 @@ package example
 
 import org.scalatest._
 
-class HelloSpec extends FlatSpec with Matchers {
-  "The Hello object" should "say hello" in {
-    Hello.greeting shouldEqual "hello"
+class HelloSpec extends WordSpec with MustMatchers {
+
+  "The crossover object" should {
+
+    "Return a list of strings" in {
+
+      Hello.crossover(Chromosome("111"), Chromosome("000"), 1) mustEqual List("100", "011")
+    }
+
+    "swaps input at given index" in {
+      Hello.crossover(Chromosome("1111"), Chromosome("0000"), 1) mustEqual List("1000", "0111")
+    }
   }
 }
